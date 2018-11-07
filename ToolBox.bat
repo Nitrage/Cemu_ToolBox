6d880b409e1b9dd4ea801e67b9e60d2a353d0e26
Nitrage <44818269+Nitrage@users.noreply.github.com>
2018-11-07 10:35:08 +0100

    Add files via upload

--- ToolBox.bat
+++ ToolBox.bat
@@ -5,10 +5,13 @@ set exe7zip=".\7za.exe"
 set exeWget=".\wget.exe"
 set tmpWget="%tmp%\wget.tmp"
 
-if not exist ".\.toolbox" (
- rem -= download json =-
- echo Downloading... : latest.json
- %exeWget% -q "https://api.github.com/repos/Nitrage/Cemu_ToolBox/releases/latest" -O %tmpWget%
+echo Check for Updates...
+rem -= download json =-
+echo Downloading... : latest.json
+%exeWget% -q "https://api.github.com/repos/Nitrage/Cemu_ToolBox/releases/latest" -O %tmpWget%
+
+:top
+if not exist ".toolbox" (
  rem -= search in json =-
  for /f "tokens=3 delims=:" %%a in ('type %tmpWget% ^| find "browser_download_url"') do (
   set link=https:%%a
@@ -17,10 +20,9 @@ if not exist ".\.toolbox" (
   for /f "tokens=8 delims=/" %%b in ("!link!") do (
    rem -= download and extract zip =-
    echo Downloading... : %%b
-   %exeWget% -q "!link!" -O ".\%%b" --show-progress
-   %exe7zip% x -y ".\%%b" >nul
-   rem -= delete json an zip =-
-   del %tmpWget%
+   %exeWget% -q "!link!" -O "%%b" --show-progress
+   %exe7zip% x -y "%%b" >nul
+   rem -= delete zip =-
    del %%b
   )
  )
@@ -29,5 +31,25 @@ if not exist ".\.toolbox" (
 call ".toolbox\index.bat"
 
 if exist "%dirCurrent%\%dirScript%" (
- call %batMenu%
+ rem -= search in config =-
+ for /f "tokens=2 delims= " %%a in ('type %txtConfig% ^| find "tb_version"') do (
+  rem -= search in json =-
+  for /f "tokens=7 delims=/" %%b in ('type %tmpWget% ^| find "browser_download_url"') do (
+   if "%%a" equ "%%b" (
+    rem -= delete json =-
+    del %tmpWget%
+    echo.
+    echo Up to date     : ToolBox_%%a
+	echo.
+	timeout /t 1 >nul
+    call %batMenu%
+   )
+   if "%%a" neq "%%b" (
+	rem -= delete old .toolbox =-
+    echo Uninstall      : ToolBox_%%a
+    rmdir /S /Q "%dirCurrent%\%dirScript%"
+    goto top
+   )
+  )
+ )
 )
